b214ecaed5ff0bfadc83396305ba4d71f8d079e6
DEV04 <dev04@ADSI0225.local>
2022-02-09 11:11:25 +0800

    done Task E

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,10 +8,19 @@ class Product < ApplicationRecord
     validates :image_url, allow_blank: true, format: {
         with: %r{\.(gif|jpg|png)\Z}i,
         message: 'must be a URL for GIF, JPG or PNG image.'
-    }
+    }    
+    #
+    ## MODEL RELATIONSHIPS
+    #
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
 
-    
-    #
-    ##
-    #
+    private
+        # ensure that there are no line items referencing this product
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                errors.add(:base, 'Line Items present')
+                throw :abort
+            end
+        end
 end
